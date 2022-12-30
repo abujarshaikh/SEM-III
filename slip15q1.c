21cd11625bf41e1c79f1225d5361e62e73d8307d
abujarshaikh <103555266+abujarshaikh@users.noreply.github.com>
2022-12-30 13:59:37 +0530

    Add files via upload

---
+++ slip15q1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j,n,loc,min,temp,a[100];
+    printf("Enter The Size Of An Array::");
+    scanf("%d",&n);
+    printf("Enter The Elements Of Array\n");
+    for ( i = 0; i < n; i++)
+    {
+        printf("Array[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+    for ( i = 0; i < n-1; i++)
+    {
+        min=a[i];
+        loc=i;
+        for ( j = i+1; j < n; j++)
+        {
+            if (min>a[j])
+            {
+                min=a[j];
+                loc=j;
+            }
+            
+        }
+        temp=a[i];
+        a[i]=a[loc];
+        a[loc]=temp;
+    }
+   printf("Elements After The Sorting\n");
+   for ( i = 0; i < n; i++)
+   {
+    printf("%d\t",a[i]);
+   }
+  return 0;  
+}
